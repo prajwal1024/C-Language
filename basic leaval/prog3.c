f7df45e73877fb358a7df7af7265f40d14d1e341
prajwal1024 <prajwalsatpute2005@gmail.com>
2025-06-05 14:32:46 +0530

    area of circle

---
+++ basic leaval/prog3.c
@@ -0,0 +1,10 @@
+// find area of circle  
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("radius of a circle: ");
+    scanf("%f",&radius);
+    printf("area of circle is: %f",3.14*radius*radius);
+    return 0;
+
+}
