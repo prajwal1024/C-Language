53cf1f6b2e986a3d1f3569bc4d171c33ee1f21f9
prajwal1024 <prajwalsatpute2005@gmail.com>
2025-07-26 16:23:55 +0530

    increment operator's

---
+++ Medium leaval/prog5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+    //increment operator
+    // ++i (pre increment)
+    // i++ (post increment)
+
+    int i=1;
+    //printf("%d \n",i++); // use,then increase
+    //printf("%d ",i);
+
+
+    printf("%d \n",++i); // increase,then use
+    printf("%d ",i);
+
+        return 0;
+
+}
